1937bd178b9e548eb2880b820f619db5f5bee221
shigenori miyazaki <s1i1g0e3@gmail.com>
2020-05-30 16:17:15 +0000

    kadai-microposts

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,13 @@ Rails.application.routes.draw do
     end
   end
   
+  resources :users, only: [:index, :show, :new, :create] do
+    member do
+      get :likes
+    end
+  end
+  
   resources :microposts, only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy]
 end
